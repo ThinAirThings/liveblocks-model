ef91be200b2468b8a67a07e001e6d1365e2400df
irishcarbomb777 <danielclannan@gmail.com>
2023-06-27 10:34:07 -0400

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,51 @@
+import { LiveMap, LiveObject, LsonObject } from "@liveblocks/client"
+import { ContainerState } from "@thinairthings/zoom-utils"
+import {v4 as uuidv4} from 'uuid'
+
+export type NodeTypeIndex = {
+    browser: {
+        type: 'browser'
+        typeDisplayName: string
+        typeDisplayIcon: string
+        defaultProps: {
+            url: string
+        }
+    }
+    rectangle: {
+        type: 'rectangle'
+        typeDisplayName: string
+        typeDisplayIcon: string
+        defaultProps: {}
+    }
+}
+
+export type NodeId = string
+export type AirNode<T extends {[key: string]: any}={}> = LiveObject<{
+    nodeId: string
+    type: keyof NodeTypeIndex
+    state: LiveObject<T&
+        {containerState: LiveObject<ContainerState>
+    }>
+    children: LiveMap<string, AirNode<any>>
+}>
+export type ImmutableAirNode<T extends {[key: string]: any}={}> = ReturnType<AirNode<T>["toImmutable"]>
+
+export const createAirNode = <T extends LsonObject={}> ({
+    type,
+    state
+}: {
+    type: keyof NodeTypeIndex
+    state: T&{containerState: ContainerState}
+}): AirNode<T> => new LiveObject({
+    nodeId: uuidv4(),
+    type,
+    state: new LiveObject({
+        ...state,
+        containerState: new LiveObject(state.containerState)
+    }),
+    children: new LiveMap()
+})
+
+export type LiveblocksStorageModel = {
+    nodeMap: LiveMap<string, AirNode<{}>>
+}
