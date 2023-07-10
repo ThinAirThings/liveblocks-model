0ef49d04e1dd8e643a3914a4534538c66b19c53b
irishcarbomb777 <danielclannan@gmail.com>
2023-07-10 10:14:06 -0400

    mod

--- dist/hooks/useMutationNodeState.d.ts
+++ dist/hooks/useMutationNodeState.d.ts
@@ -1,17 +1,3 @@
-import { ImmutableAirNode, LiveblocksPresence, LiveblocksStorageModel } from "..";
+import { LiveblocksPresence, LiveblocksStorageModel, NodeTypeIndex } from "..";
 import { createRoomContext } from "@liveblocks/react";
-export declare const useMutationNodeState: <T extends Record<string, any>, Key extends keyof ImmutableAirNode<T>["state"]>(useMutation: ReturnType<typeof createRoomContext<LiveblocksPresence, LiveblocksStorageModel>>['suspense']['useMutation'], nodeId: string, key: Key) => (value: ({
-    nodeId: string;
-    type: keyof import("..").NodeTypeIndex;
-    state: import("@liveblocks/core").LiveObject<T & {
-        containerState: import("@liveblocks/core").LiveObject<import("@thinairthings/zoom-utils").ContainerState>;
-    }>;
-    children: import("@liveblocks/core").LiveMap<string, import("..").AirNode<any>>;
-} extends infer T_1 ? T_1 extends {
-    nodeId: string;
-    type: keyof import("..").NodeTypeIndex;
-    state: import("@liveblocks/core").LiveObject<T & {
-        containerState: import("@liveblocks/core").LiveObject<import("@thinairthings/zoom-utils").ContainerState>;
-    }>;
-    children: import("@liveblocks/core").LiveMap<string, import("..").AirNode<any>>;
-} ? T_1 extends import("@liveblocks/core").LsonObject ? T_1 extends infer T_2 extends import("@liveblocks/core").LsonObject ? { readonly [K in keyof T_2]: import("@liveblocks/core").ToImmutable<Exclude<T_1[K], undefined>> | (undefined extends T_1[K] ? T_1[K] & undefined : never); } : never : T_1 extends import("@liveblocks/core").Json ? T_1 : never : never : never)["state"][Key]) => void;
+export declare const useMutationNodeState: <T extends keyof NodeTypeIndex, K extends keyof NodeTypeIndex[T]["defaultProps"]>(useMutation: ReturnType<typeof createRoomContext<LiveblocksPresence, LiveblocksStorageModel>>['suspense']['useMutation'], nodeId: string, key: K) => (value: NodeTypeIndex[T]["defaultProps"][K]) => void;
--- src/hooks/useMutationNodeState.ts
+++ src/hooks/useMutationNodeState.ts
@@ -1,14 +1,14 @@
-import { ImmutableAirNode, LiveblocksPresence, LiveblocksStorageModel } from ".."
+import { ImmutableAirNode, LiveblocksPresence, LiveblocksStorageModel, NodeTypeIndex } from ".."
 import {createRoomContext} from "@liveblocks/react"
 export const useMutationNodeState = <
-    T extends Record<string, any>, 
-    Key extends keyof ImmutableAirNode<T>['state']
+    T extends keyof NodeTypeIndex,
+    K extends keyof NodeTypeIndex[T]['defaultProps']
 ,>(
     useMutation: ReturnType<typeof createRoomContext<LiveblocksPresence, LiveblocksStorageModel>>['suspense']['useMutation'],
     nodeId: string,
-    key: Key
+    key: K
 ) => {
-    return useMutation(({storage}, value: ImmutableAirNode<T>['state'][Key]) => {
+    return useMutation(({storage}, value: NodeTypeIndex[T]['defaultProps'][K]) => {
         storage.get("nodeMap")!.get(nodeId)!.get("state")!.set(key as any, value)
     }, [])
 }
