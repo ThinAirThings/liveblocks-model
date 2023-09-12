461cc7e18e6592ce04c356259b4ee3027e583156
irishcarbomb777 <danielclannan@gmail.com>
2023-09-12 12:48:27 -0400

    mod

---
+++ src/environments/shared/mutations/useMutationUpdateMetaFactory.ts
@@ -0,0 +1,17 @@
+import { Lson } from "@liveblocks/client";
+import { LiveAirNode } from "../../../index.node.js";
+import { MutationHook } from "../hook-types.js";
+
+
+
+export const useMutationUpdateMetaFactory = <
+    LiveAirNodeUnion extends LiveAirNode<any, any>,
+    Meta extends Lson
+>(
+    useMutation: MutationHook<LiveAirNodeUnion, Meta>
+) => useMutation((
+    {storage},
+    updater: (meta: Meta) => void
+)=> {
+    updater(storage.get('meta'))
+}, []) 
